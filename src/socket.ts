d82d18e3cd1ca50f089f2cd6c827477b2e31383a
Vinicius Paes Berna <vini_berna2.0@hotmail.com>
2021-10-24 02:58:58 -0300

    fix: CORS origins

--- src/socket.ts
+++ src/socket.ts
@@ -4,7 +4,7 @@ import { Server, Socket } from "socket.io";
 export default (httpServer: any) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: ["http://localhost:3000", "https://drakest.vercel.app/"]
+      origin: ["http://localhost:3000/*", "https://drakest.vercel.app/*"]
     }
   })
 
