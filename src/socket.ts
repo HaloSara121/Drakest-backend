61ef65730a519d15202511d6e1966ea029d2530b
Vinicius Paes Berna <vini_bern2.0@hotmail.com>
2022-02-26 14:32:58 -0300

    feat: Procfile added

--- src/socket.ts
+++ src/socket.ts
@@ -8,11 +8,13 @@ export default (httpServer: any) => {
     }
   })
 
-  if(process.env.NODE_ENV === "production") {
-    useSocketServer(io, { controllers: [__dirname + '/api/controllers/*.js'] })  
-  } else {
+  // if(process.env.NODE_ENV === "production") {
+  //   useSocketServer(io, { controllers: [__dirname + '/api/controllers/*.js'] })  
+  // }
+
+  // if(process.env.NODE_ENV === "development") {
     useSocketServer(io, { controllers: [__dirname + '/api/controllers/*.ts'] })  
-  }
+  // } 
 
   return io
 }
